931950f0284a289f37d8beaea81bedb0d7e06ffc
Federico Amedeo Izzo <federico.izzo42@gmail.com>
2016-05-28 19:43:39 +0200

    Added pin button check

--- HotSpot/HotSpot.ino
+++ HotSpot/HotSpot.ino
@@ -1,3 +1,5 @@
+#include <Ticker.h>
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
@@ -11,14 +13,19 @@
 
 #define API_URL_AUTH "https://" API_URL "/" API_AUTH "/"
 
-#define KEY_1_PIN D1
-#define KEY_2_PIN D2
-#define KEY_3_PIN D3
+const int KEYPIN[3] = {D1,D2,D3};
 
 #define LOCK_PIN D4
 #define SERVO_PIN D5
 #define RLED_PIN D6
 #define GLED_PIN D7
+
+bool locked = false;
+int currdigit = 0;
+int secret[4] = {1,2,3,1};
+int combination[4] = {0,0,0,0};
+
+Ticker callAfterSecs;
 
 void sendRequest(const char *request)
 {
@@ -34,23 +41,67 @@ void sendRequest(const char *request)
   if (httpCode > 0) {
     Serial.print(httpCode);
     Serial.print(": ");
-    Serial.println(http.getString());  
+    Serial.println(http.getString());
   }
   else {
     Serial.print("HTTP GET... failed: ");
     Serial.println(http.errorToString(httpCode));
   }
 }
+void enableInterrupts()
+{
+  attachInterrupt(digitalPinToInterrupt(KEYPIN[0]), key1handler, FALLING);
+  attachInterrupt(digitalPinToInterrupt(KEYPIN[1]), key2handler, FALLING);
+  attachInterrupt(digitalPinToInterrupt(KEYPIN[2]), key3handler, FALLING);
+}
+void handleButtons(int key)
+{
+  //Disable interrupts to avoid input bouncing
+  detachInterrupt(digitalPinToInterrupt(KEYPIN[key]));
+  //Complete debouncing by re-enabling interrupts
+  callAfterSecs.once(0.5, enableInterrupts);
+
+  Serial.printf("Digit %d = %d", currdigit, key+1);
+
+  combination[currdigit] = key+1;
+  Serial.printf("Saved key %d", combination[currdigit]);
+
+  if(currdigit < 3) {
+    currdigit++;
+  }
+  else {
+    currdigit=0;
+    if(checkSequence()) {
+      locked = false;
+      Serial.println("Unlocked!");
+    }
+  }
+}
+bool checkSequence()
+{
+  bool match = true;
+  for(int digit = 0;digit < 4; digit++) {
+    if(combination[digit] != secret[digit]) {
+      match = false;
+      Serial.println("Wrong combination");
+      break;
+    }
+  }
+  return match;
+}
+void key1handler(){ handleButtons(0); }
+void key2handler(){ handleButtons(1); }
+void key3handler(){ handleButtons(2); }
 
 void setup() {
-  pinMode(KEY1_PIN, INPUT_PULLUP);
-  pinMode(KEY2_PIN, INPUT_PULLUP);
-  pinMode(KEY3_PIN, INPUT); //On-board pullup
+  pinMode(KEYPIN[0], INPUT_PULLUP);
+  pinMode(KEYPIN[1], INPUT_PULLUP);
+  pinMode(KEYPIN[2], INPUT); //On-board pullup
   pinMode(LOCK_PIN, INPUT); //On-board pullup
   pinMode(SERVO_PIN, OUTPUT);
   pinMode(RLED_PIN, INPUT_PULLUP);
   pinMode(GLED_PIN, INPUT_PULLUP);
-  
+
   Serial.begin(115200);
   WiFi.begin(SSID, PASSWORD);
   Serial.print("Connessione in corso");
@@ -75,6 +126,10 @@ void loop() {
 
   sprintf(request, "%ssendLocation?chat_id=%d&latitude=%s&longitude=%s", API_URL_AUTH, chat_id, latitude, longitude);
   sendRequest(request);
+
+
+  enableInterrupts();
+  locked = true;
 
   delay(30000);
 }
